0c6962c4a70ca25386ff6d79fc96a7e1c138b882
Andrei Luca <1881266+iamandrewluca@users.noreply.github.com>
2025-01-25 13:53:44 +0200

    chore: switch to lib mode

--- packages/vue-inline-svg/vite.config.ts
+++ packages/vue-inline-svg/vite.config.ts
@@ -1,7 +1,18 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	build: {
+		lib: {
+			entry: resolve(import.meta.dirname, "lib/vue-inline-svg.vue"),
+			fileName: "vue-inline-svg",
+			formats: ["es"],
+		},
+		rollupOptions: {
+			external: ["vue", "html-parse-stringify"],
+		},
+	},
 });
